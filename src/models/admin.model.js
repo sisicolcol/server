f515d731c45ca7ef454a56aa553010d6e0f28d0c
heesoo <11junghs@gmail.com>
2022-08-27 20:36:51 +0900

    [0827] nodemon 추가

--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -3,7 +3,8 @@ class AdminModel {
     cosntructor(){}
 
     selectUserList = async (conn, whereQuery, offset) => {
-        const adminSelectUserQuery = `SELECT * FROM user WHERE 1 = 1`;
+        const adminSelectUserQuery = `SELECT * FROM user`;
+        // const adminSelectUserQuery = `SELECT * FROM user WHERE 1 = 1`;
         const offsetQuery = ` limit 10 offset ?`;
         const [userRow] = await conn.query(adminSelectUserQuery+whereQuery+offsetQuery, offset);
     
