d9d3696a374bf52dfcb49a9fe88c44315c7e1b3e
Suanna01 <yangnony01@gmail.com>
2022-09-04 22:21:04 +0900

    [add] complete controller, service

--- src/controllers/hp.apply.controller.js
+++ src/controllers/hp.apply.controller.js
@@ -12,32 +12,71 @@ class HpApplyController {
 
     // 헬퍼 퀵/사전 활동 지원 서비스) 지원하기 -기존 자기소개서 보기
     getHpApply = async (req,res)=>{
+        const hp_id = req.params.hp_id;
 
-    }
+        if(!hp_id){
+            return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
+        } else if (hp_id <= 0) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
+        }
 
-    // 헬퍼 새로운 자기소개서 작성하기
-    postHpApplyNewIdc = async(req,res)=>{
+        const Result = await this.HpApplyService.retrieveHpApply(hp_id);
 
+        return res.send(response(baseResponse.SUCCESS, Result));
     }
     
-    // 헬퍼 지원 완료하기 -> 지원한 신청서의 시간까지 post
+    // 헬퍼 지원 완료하기 -> 지원한 신청서의 시간까지 post해서 알림 목록으로
     postHpApply = async(req,res)=>{
+        const {apply_id, mem_id, hp_id, hp_dic_id, is_new, new_idc, is_success, memo, apply_date, start_point, end_point} = req.body;
+
+        const Result = await this.HpApplyService.completeHpApply(
+            apply_id, mem_id, hp_id, hp_dic_id, is_new, new_idc, is_success, memo, apply_date, start_point, end_point
+        );
+
+        return res.send(response(baseResponse.SUCCESS));
 
     }
 
     // 헬퍼 지원 목록) pg_id 내림차순 (업로드 순)
     getHpApplyList = async(req,res)=>{
+        const hp_id = req.params.hp_id;
 
+        if(!hp_id){
+            return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
+        } else if (hp_id <= 0) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
+        }
+
+        const Result = await this.HpApplyService.retrieveHpApplyList(hp_id);
+
+        return res.send(response(baseResponse.SUCCESS, Result));
     }
 
     // 헬퍼 지원 목록) 공고 자세히 보기
     getHpApplyDetail = async(req,res)=>{
+        const apply_id = req.params.apply_id;
+
+        if(!apply_id){
+            return res.send(errResponse(baseResponse.POST_POSTIDX_EMPTY));
+        } else if (apply_id <= 0) {
+            return res.send(errResponse(baseResponse.POST_POSTIDX_LENGTH));
+        }
+
+        const Result = await this.HpApplyService.retrieveHpApplyDetail(apply_id);
+
+        return res.send(response(baseResponse.SUCCESS, Result));
 
     }
 
     // 헬퍼 마이페이지) 프로필 및 자기소개서 설정
     postHpProfile = async(req,res)=>{
+        const {hp_id, hp_name, hp_idc_id, content} = req.body;
 
+        const Result = await this.HpApplyService.settingHpProfile(
+            hp_id, hp_name, hp_idc_id, content
+        );
+
+        return res.send(response(baseResponse.SUCCESS));
     }
 
 }
--- src/services/hp.apply.service.js
+++ src/services/hp.apply.service.js
@@ -13,6 +13,82 @@ class HpApplyService {
         this.HpApplyModel = new HpApplyModel();
     }
 
+    
+    // 헬퍼 퀵/사전 활동 지원 서비스) 지원하기 -기존 자기소개서 보기
+    retrieveHpApply = async (hp_id) => {
+        const connection = await pool.getConnection(async (connection)=>connection);
+        try {
+            await connection.beginTransaction();
+
+        } catch (error) {
+            console.log(error);
+            await connection.rollback();
+            return errResponse(baseResponse.DB_ERROR);
+        } finally {
+            connection.release();
+        }
+    }
+    
+    // 헬퍼 지원 완료하기 -> 지원한 신청서의 시간까지 post해서 알림 목록으로
+    completeHpApply = async (apply_id, mem_id, hp_id, hp_dic_id, is_new, new_idc, is_success, memo, apply_date, start_point, end_point) => {
+        const connection = await pool.getConnection(async (connection)=>connection);
+        try {
+            await connection.beginTransaction();
+            
+        } catch (error) {
+            console.log(error);
+            await connection.rollback();
+            return errResponse(baseResponse.DB_ERROR);
+        } finally {
+            connection.release();
+        }
+    }
+
+    // 헬퍼 지원 목록) pg_id 내림차순 (업로드 순)
+    retrieveHpApplyList = async (hp_id) => {
+        const connection = await pool.getConnection(async (connection)=>connection);
+        try {
+            await connection.beginTransaction();
+            
+        } catch (error) {
+            console.log(error);
+            await connection.rollback();
+            return errResponse(baseResponse.DB_ERROR);
+        } finally {
+            connection.release();
+        }
+    }
+
+    // 헬퍼 지원 목록) 공고 자세히 보기
+    retrieveHpApplyDetail = async (apply_id) => {
+        const connection = await pool.getConnection(async (connection)=>connection);
+        try {
+            await connection.beginTransaction();
+            
+        } catch (error) {
+            console.log(error);
+            await connection.rollback();
+            return errResponse(baseResponse.DB_ERROR);
+        } finally {
+            connection.release();
+        }
+    }
+
+    // 헬퍼 마이페이지) 프로필 및 자기소개서 설정
+    settingHpProfile = async (hp_id, hp_name, hp_idc_id, content) => {
+        const connection = await pool.getConnection(async (connection)=>connection);
+        try {
+            await connection.beginTransaction();
+            
+        } catch (error) {
+            console.log(error);
+            await connection.rollback();
+            return errResponse(baseResponse.DB_ERROR);
+        } finally {
+            connection.release();
+        }
+    }
+
 }
 
 module.exports = HpApplyService;
